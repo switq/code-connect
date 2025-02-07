4a4ebd472ecbf83a5515032cf1cef5261c7fdf54
switq <switchaep@gmail.com>
2025-02-07 11:24:21 -0300

    Create next app

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Code Connect",
+  description: "Uma rede social para devs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.module.css.d.ts
@@ -0,0 +1,5 @@
+// This file is generated. NEVER MODIFY IT!
+declare const page: {
+};
+export default page;
+//# sourceMappingURL=page.module.css.d.ts.map
