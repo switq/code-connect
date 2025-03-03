76dc7b82ae4a6cd235cc652218eeed167f3e82de
switq <switchaep@gmail.com>
2025-03-03 14:29:28 -0300

    Add global styles and Aside component for layout enhancement

---
+++ src/app/components/Aside/asisde.module.css.d.ts
@@ -0,0 +1,6 @@
+// This file is generated. NEVER MODIFY IT!
+declare const asisde: {
+  readonly aside: string;
+};
+export default asisde;
+//# sourceMappingURL=asisde.module.css.d.ts.map
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { Aside } from './components/Aside';
+import './globals.css'
+
 export const metadata = {
   title: "Code Connect",
   description: "Uma rede social para devs",
@@ -7,7 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        {children}
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
       </body>
     </html>
   );
